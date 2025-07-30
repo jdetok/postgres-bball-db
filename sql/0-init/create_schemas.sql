b4dda963735872df016a4461f67e35b0b585461b
jdetok <jdeko17@gmail.com>
2025-07-29 20:02:58 -0500

    basic league player team architecture

--- sql/0-init/create_schemas.sql
+++ sql/0-init/create_schemas.sql
@@ -4,6 +4,8 @@ create schema intake;
 -- api view tables, will need to match the mariadb ones
 create schema api;
 
+create schema league;
+
 -- player tables
 create schema player;
 
---
+++ sql/1-tbl/player.sql
@@ -0,0 +1,59 @@
+create table league.lg (
+    lg_id int primary key,
+    lg varchar(10),
+    lg_name varchar(255)
+);
+
+create table league.team (
+    team_id bigint primary key,
+    lg_id int references league.lg(lg_id),
+    team varchar(10),
+    team_name varchar(255),
+    team_city varchar(255)
+);
+
+create table player.all (
+    player_id bigint primary key,
+    player varchar(255),
+    last_first varchar(255),
+    lg_id int references league.lg(lg_id)
+);
+
+create table player.current (
+    player_id bigint primary key references player.all(player_id),
+    player varchar(255),
+    last_first varchar(255),
+    team_id bigint references league.team(team_id),
+    lg_id int references league.lg(lg_id)
+);
+
+create table player.box (
+    game_id bigint not null,
+    team_id bigint not null references league.team(team_id),
+    lg_id int references league.lg(lg_id),
+    player_id bigint not null references player.all(player_id),
+    player varchar(255),
+    matchup varchar(50),
+    wl varchar(1),
+    mins int,
+    pts int,
+    ast int,
+    reb int,
+    stl int,
+    blk int,
+    tov int,
+    oreb int,
+    dreb int,
+    foul int,
+    pm int,
+    fgm int,
+    fga int,
+    fgp numeric(5, 4),
+    f3m int,
+    f3a int,
+    f3p numeric(5, 4),
+    ftm int,
+    fta int,
+    ftp numeric(5, 4),
+    primary key (game_id, player_id)
+);
